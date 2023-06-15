8f1d274a9cd0e2019e5b18613ac044584a10ec34
birddrink <birddrinkgaming@gmail.com>
2023-06-16 00:14:03 +0300

    Initial commit

---
+++ src/main/java/net/birddrink/birddrinkmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.birddrink.birddrinkmod.item;
+
+import net.birddrink.birddrinkmod.BirddrinkMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BirddrinkMod.MOD_ID);
+    public static final RegistryObject<Item> RAINBONIUM = ITEMS.register("rainbonium",
+            ()-> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> RAW_RAINBONIUM = ITEMS.register("raw_rainbonium",
+            ()-> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register((eventBus));
+    }
+}
